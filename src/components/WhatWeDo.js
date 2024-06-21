5e22fc6011b47c91daf5e43e008f4fd652962305
joshbam-joshwilly <j.alana@alustudent.com>
2024-06-21 23:41:56 +0200

    to be continued tomorrow

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -29,117 +29,117 @@ const WhatWeDo = () => {
             viewport={{ once: false }}
             variants={staggerContainer}
         >
-            <motion.h1 className="text-4xl font-bold mb-6 text-center text-white" variants={fadeInUp}>What We Do</motion.h1>
-            <motion.h2 className="text-white text-center" variants={fadeInUp}>Our Services</motion.h2>
-            <motion.p className="text-center text-lg mb-8 text-gray-300" variants={fadeInUp}>
+            <motion.h1 className="text-4xl font-bold mb-6 text-center text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>What We Do</motion.h1>
+            <motion.h2 className="text-white text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Our Services</motion.h2>
+            <motion.p className="text-center text-lg mb-8 text-gray-300" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                 We offer a variety of exceptional services to meet your needs and ensure you have the best experience possible.
             </motion.p>
             <Carousel showThumbs={true} showStatus={false} infiniteLoop autoPlay>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Game Day Travel</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Events</h2>
                         <p className="text-base mb-4">
                             We aim to provide exceptional service with our state-of-the-art vehicles, equipped with the latest amenities and technology.
                             Our dedicated team ensures every detail is handled, offering a seamless and luxurious journey.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Airport Transfers</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">General Private Travel</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Corporate Travel</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Weddings</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
                 <motion.div className="flex justify-center items-center space-x-4 h-auto py-10" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <div className="w-1/3 h-80">
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={leftImage} alt="Left Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <div className="flex-1 text-center px-6 text-white">
+                    </motion.div>
+                    <motion.div className="flex-1 text-center px-6 text-white" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <h2 className="text-3xl font-semibold mb-4">Exotic Aviation</h2>
                         <p className="text-base mb-4">
                             We provide exceptional service to ensure you have the best experience possible.
                         </p>
                         <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700">Learn More</button>
-                    </div>
-                    <div className="w-1/3 h-80">
+                    </motion.div>
+                    <motion.div className="w-1/2 h-70" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                         <img src={rightImage} alt="Right Image" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
+                    </motion.div>
                 </motion.div>
             </Carousel>
         </motion.div>
--- src/components/WhatYouCanExpect.js
+++ src/components/WhatYouCanExpect.js
@@ -32,42 +32,56 @@ const WhatYouCanExpect = () => {
             </motion.p>
             <motion.div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-12" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
                 <motion.div className="flex-1 text-center md:text-left" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                    <h2 className="text-3xl font-semibold mb-4">Features of Our Luxury Cars</h2>
+                    <motion.h2 className="text-3xl font-semibold mb-4" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Features of Our Luxury Cars</motion.h2>
                     <motion.div className="grid grid-cols-1 md:grid-cols-2 gap-4" variants={staggerContainer}>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaPhone className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">Partition Cabin with Intercom</h3>
-                            <p className="text-sm text-center">For that extra element of exclusivity and privacy whilst knowing your driver is a touch of a button away.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaPhone className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Partition Cabin with Intercom</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>For that extra element of exclusivity and privacy whilst knowing your driver is a touch of a button away.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaRegSnowflake className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">Refrigerated Refreshments</h3>
-                            <p className="text-sm text-center">It wouldn’t be luxury without chilled refreshments to add that extra touch of comfort.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaRegSnowflake className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Refrigerated Refreshments</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>It wouldn’t be luxury without chilled refreshments to add that extra touch of comfort.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaPlug className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">Multi Cabin Charging Functionality</h3>
-                            <p className="text-sm text-center">Stay charged up with plenty of Cabin charging points.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaPlug className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Multi Cabin Charging Functionality</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Stay charged up with plenty of Cabin charging points.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaWifi className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">In Cabin Wifi Functionality</h3>
-                            <p className="text-sm text-center">Stay connected with our high-speed and reliable Cabin WiFi.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaWifi className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>In Cabin Wifi Functionality</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Stay connected with our high-speed and reliable Cabin WiFi.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaTv className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">In Cabin Entertainment</h3>
-                            <p className="text-sm text-center">Stay entertained with In Cabin entertainment such as AppleTV, SkySports, Youtube or even sit back and get into your favourite Netflix series.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaTv className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>In Cabin Entertainment</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Stay entertained with In Cabin entertainment such as AppleTV, SkySports, Youtube or even sit back and get into your favourite Netflix series.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaWhatsapp className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">Communication</h3>
-                            <p className="text-sm text-center">Stay in touch as a member via Whatsapp, our team are on hand to attend to your requirements and arrange your next journey with us.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaWhatsapp className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Communication</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Stay in touch as a member via Whatsapp, our team are on hand to attend to your requirements and arrange your next journey with us.</motion.p>
                         </motion.div>
                         <motion.div className="bg-gray-900 p-6 rounded-lg shadow-md flex flex-col items-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
-                            <FaHandshake className="text-red-600 text-5xl mb-2" />
-                            <h3 className="text-lg font-bold mb-2">Meet & Greet</h3>
-                            <p className="text-sm text-center">We’re at your service, whether its tracking your flight and meeting you at arrivals or finding the most convenient parking space to make your life that touch easier.</p>
+                            <motion.div variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>
+                                <FaHandshake className="text-red-600 text-5xl mb-2" />
+                            </motion.div>
+                            <motion.h3 className="text-lg font-bold mb-2" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>Meet & Greet</motion.h3>
+                            <motion.p className="text-sm text-center" variants={fadeInUp} whileInView="visible" viewport={{ once: false }}>We’re at your service, whether its tracking your flight and meeting you at arrivals or finding the most convenient parking space to make your life that touch easier.</motion.p>
                         </motion.div>
                     </motion.div>
                 </motion.div>
