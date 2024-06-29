7685b6da549090347479559dca42b993df736180
joshbam-joshwilly <j.alana@alustudent.com>
2024-06-29 17:46:47 +0200

    new changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,11 @@ import logo from '../images/logo.png';
 
 const Footer = () => {
     return (
-        <footer className="bg-[#470A1C] text-white py-10 px-6">
+        <footer className="bg-[#470A1C] text-white py-6 px-6 pb-16 md:py-10">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div className="text-center md:text-left">
                     <img src={logo} alt="Company Logo" className="h-16 mx-auto md:mx-0" />
                     <p className="mt-4">
-                        Exotic Wheels<br />
                         Travel in style and comfort
                     </p>
                 </div>
@@ -23,7 +22,7 @@ const Footer = () => {
                         <li><Link to="/contact" className="hover:text-gray-400">Contact</Link></li>
                     </ul>
                 </div>
-                <div className="text-center md:text-left">
+                <div className="hidden md:block text-center md:text-left">
                     <p className="text-lg font-bold mb-4">Connect with us:</p>
                     <div className="flex justify-center md:justify-start space-x-4 mt-2">
                         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
@@ -39,7 +38,15 @@ const Footer = () => {
                     <p className="mt-4">
                         &copy; {new Date().getFullYear()} Exotic Wheels<br />
                         All rights reserved.<br />
-                        Powered by CREIT TECHNOLOGIES LIMITED
+                        Powered by CREIT TECHNOLOGIES LIMITED<br />
+                        Designed by Alivat Inc
+                    </p>
+                </div>
+                <div className="block md:hidden text-center">
+                    <p className="mt-4">
+                        &copy; {new Date().getFullYear()} Exotic Wheels<br />
+                        All rights reserved.<br />
+                        Designed and developed by CREIT DIGITALS
                     </p>
                 </div>
             </div>
