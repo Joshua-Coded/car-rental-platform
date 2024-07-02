5cd11bdcd5c161d61d9ced9f29586d01bfa3943b
joshbam-joshwilly <j.alana@alustudent.com>
2024-07-02 09:54:56 +0200

    some changes

--- src/components/ComingSoonComponent .js
+++ src/components/ComingSoonComponent .js
@@ -24,13 +24,13 @@ const ComingSoonComponent = () => {
             </div>
 
 
-            <div className="w-full md:w-1/2 flex flex-col items-center md:items-start mt-4 md:mt-0 md:ml-8">
+            <div className="w-full md:w-1/2 flex flex-col items-center mt-4 md:mt-0 md:ml-8">
                 <h1 className="text-lg text-center md:text-2xl font-bold mb-4">Exotic Aviation</h1>
-                <div className="flex items-center text-center mb-4">
-                    <span className="text-lg  md:text-xl font-semibold">Coming Soon!</span>
-                    <div className="border-l-4 border-gray-300 h-20 mx-4"></div>
+                <div className="flex flex-col items-center text-center mb-4">
+                    <span className="text-lg md:text-xl font-semibold">Coming Soon!</span>
+                    <div className="border-l-4 border-gray-300 h-20 mx-4 my-2"></div>
                 </div>
-                <p className="text-gray-700 text-justify leading-relaxed">
+                <p className="text-gray-700 text-justify leading-relaxed text-center">
                     Flying private with Exotic Wheels allows you to tailor every aspect of your journey to your preferences. <br /> Choose your departure time and destination, select the countries you wish to visit, and enjoy gourmet meals onboard.
                     We ensure that your flight is an integral part of your travel experience, offering relaxation from the moment you step on board until you return home.
                     <br /> <br />Our mission at Exotic Wheels is to provide first-class travel, ensuring our clients enjoy the utmost comfort and luxury. Experience unforgettable adventures with friends and loved ones.
